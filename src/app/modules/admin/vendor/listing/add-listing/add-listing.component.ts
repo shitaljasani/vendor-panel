7ca64866f5322e309c75835e57d0d5b16b5994dc
shitaljasani <shitaljasani302@gmail.com>
2021-06-25 00:40:40 +0530

    add listing page design

--- src/app/modules/admin/vendor/listing/add-listing/add-listing.component.ts
+++ src/app/modules/admin/vendor/listing/add-listing/add-listing.component.ts
@@ -8,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class AddListingComponent implements OnInit {
 
   constructor() { }
-
+  addProduct=[{
+    sku:"",
+    Price:"",
+    varition:""
+  }]
   ngOnInit(): void {
   }
 
+addata(){
+  this.addProduct.push({
+    sku:"",
+    Price:"",
+    varition:""
+  })
+}
 }
